2208dc7d6c787199f1d702cf042219c41a27341e
Tom Ganley <tganley@bleacherreport.com>
2020-01-21 08:28:48 -0800

    Finish Trie chapter.

--- DSAndAlgorithmsWithDebug/Trie/Trie.swift
+++ DSAndAlgorithmsWithDebug/Trie/Trie.swift
@@ -29,4 +29,49 @@ class Trie<CollectionType: Collection> where CollectionType.Element: Hashable {
         
         return current.isTerminating
     }
+
+    func remove(_ collection: CollectionType) {
+        var current = root
+        for element in collection {
+            guard let child = current.children[element] else {
+                return
+            }
+            current = child
+        }
+        guard current.isTerminating else { return }
+
+        current.isTerminating = false
+        while let parent = current.parent, current.children.isEmpty && !current.isTerminating {
+            parent.children[current.key!] = nil
+            current = parent
+        }
+    }
+}
+
+extension Trie where CollectionType: RangeReplaceableCollection {
+    func collections(startingWith prefix: CollectionType) -> [CollectionType] {
+        var current = root
+        for element in prefix {
+            guard let child = current.children[element] else { return [] }
+            current = child
+        }
+
+        return collections(startingWith: prefix, after: current)
+    }
+
+    private func collections(startingWith prefix: CollectionType, after node: Node) -> [CollectionType] {
+        var results: [CollectionType] = []
+
+        if node.isTerminating {
+            results.append(prefix)
+        }
+
+        for child in node.children.values {
+            var prefix = prefix
+            prefix.append(child.key!)
+            results.append(contentsOf: collections(startingWith: prefix, after: child))
+        }
+
+        return results
+    }
 }
--- DSAndAlgorithmsWithDebug/Trie/TrieChallenges.swift
+++ DSAndAlgorithmsWithDebug/Trie/TrieChallenges.swift
@@ -3,8 +3,18 @@ class TrieChallenges {
     static func playground() {
         let trie = Trie<String>()
         trie.insert("cute")
-        if trie.contains("cute") {
-            print("cute is in the trie")
-        }
+        trie.insert("cut")
+
+        print("\n*** Before removing ***")
+        assert(trie.contains("cut"))
+        print("cut is in the trie")
+        assert(trie.contains("cute"))
+        print("cute is in the trie")
+
+        print("\n*** After removing cut ***")
+        trie.remove("cut")
+        assert(!trie.contains("cut"))
+        assert(trie.contains("cute"))
+        print("cute is still in the trie")
     }
 }
