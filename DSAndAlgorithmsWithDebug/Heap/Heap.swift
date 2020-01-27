6855affceea48bf92e7859e0d6c54d7f3844e9c0
Tom Ganley <tganley@bleacherreport.com>
2020-01-27 08:55:21 -0800

    WIP heap

--- DSAndAlgorithmsWithDebug/Heap/Heap.swift
+++ DSAndAlgorithmsWithDebug/Heap/Heap.swift
@@ -5,4 +5,54 @@ struct Heap<Element: Equatable> {
     init(sort: @escaping (Element, Element) -> Bool) {
         self.sort = sort
     }
+
+    var isEmpty: Bool {
+        return elements.isEmpty
+    }
+
+    var count: Int {
+        return elements.count
+    }
+
+    func peek() -> Element? {
+        return elements.first
+    }
+
+    func leftChildIndex(ofParentAt index: Int) -> Int {
+        return 2 * index + 1
+    }
+
+    func rightChildIndex(ofParentAt index: Int) -> Int {
+        return 2 * index + 2
+    }
+
+    func parentIndex(ofChildAt index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    mutating func remove() -> Element? {
+        guard !isEmpty else {
+            return nil
+        }
+
+        elements.swapAt(0, count - 1)
+        defer {
+            siftDown(from: 0)
+        }
+
+        return elements.removeLast()
+    }
+
+    mutating func siftDown(from index: Int) {
+        var parent = index
+        while true {
+            let left = leftChildIndex(ofParentAt: parent)
+            let right = rightChildIndex(ofParentAt: parent)
+            var candidate = parent
+
+            if left < count && sort(elements[left], elements[candidate]) {
+                
+            }
+        }
+    }
 }
