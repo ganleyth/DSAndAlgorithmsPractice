413754043bed64c6d9cc0af040717873602b85fd
Tom <ganleyapps@gmail.com>
2020-01-27 07:44:43 -0800

    WIP Heap

---
+++ DSAndAlgorithmsWithDebug/Heap/Heap.swift
@@ -0,0 +1,8 @@
+struct Heap<Element: Equatable> {
+    var elements: [Element] = []
+    let sort: (Element, Element) -> Bool
+    
+    init(sort: @escaping (Element, Element) -> Bool) {
+        self.sort = sort
+    }
+}
