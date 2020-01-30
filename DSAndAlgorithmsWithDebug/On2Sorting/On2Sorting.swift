91a979c7fb673784ff9f69aebecc0f2a91403c7a
Tom <ganleyapps@gmail.com>
2020-01-29 23:32:10 -0800

    WIP O(n^2) sorting.

--- DSAndAlgorithmsWithDebug/On2Sorting/On2Sorting.swift
+++ DSAndAlgorithmsWithDebug/On2Sorting/On2Sorting.swift
@@ -18,3 +18,59 @@ func bubbleSort<Element>(_ array: inout [Element]) where Element: Comparable {
         }
     }
 }
+
+func bubbleSort2<T>(_ collection: inout T) where T: MutableCollection, T.Element: Comparable {
+    guard collection.count >= 2 else { return }
+    
+    for end in collection.indices.reversed() {
+        var swapped = false
+        var current = collection.startIndex
+        
+        while current < end {
+            let next = collection.index(after: current)
+            if collection[current] > collection[next] {
+                collection.swapAt(current, next)
+                swapped = true
+            }
+            current = next
+        }
+        
+        if !swapped {
+            return
+        }
+    }
+}
+
+func selectionSort<Element>(_ array: inout [Element]) where Element: Comparable {
+    guard array.count >= 2 else {
+        return
+    }
+    
+    for current in 0..<(array.count - 1) {
+        var lowest = current
+        
+        for other in (current + 1)..<array.count {
+            if array[lowest] > array[other] {
+                lowest = other
+            }
+        }
+        
+        if lowest != current {
+            array.swapAt(lowest, current)
+        }
+    }
+}
+
+func insertionSort<Element>(_ array: inout [Element]) where Element: Comparable {
+    guard array.count >= 2 else { return }
+    
+    for current in 1..<array.count {
+        for shifting in (1...current).reversed() {
+            if array[shifting] < array[shifting - 1] {
+                array.swapAt(shifting, shifting - 1)
+            } else {
+                break
+            }
+        }
+    }
+}
